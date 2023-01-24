1144b1bd3aaaa4f30ec360e50278ab507e3e3c00
Brian D'Ostilio <briand787b@gmail.com>
2023-01-24 16:17:01 -0500

    adds ipynb for parse module'

--- autotrader/autotrader.py
+++ autotrader/autotrader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import db
+import database
 import scrape
 import time
 
@@ -13,12 +13,12 @@ def main():
     # test_scrape_doc()
     # exit(0)
 
-    eng = db.engine()
+    eng = database.engine()
     for region in scrape.all_regions():
         for target in scrape.all_targets():
             try:
                 inv_list = scrape.scrape_model(target, region)
-                db.save_listings(eng, inv_list)
+                database.save_listings(eng, inv_list)
                 time.sleep(10)
             except Exception as e:
                 print(f'failed to scrape target ({target}): {e}')
@@ -27,21 +27,21 @@ def main():
 
 
 def test_scrape_doc():
-    eng = db.engine()
+    eng = database.engine()
     with open('examples/resp.html', 'r') as file:
         doc = file.read()
 
     inv_list, _ = scrape.scrape_doc(doc)
     print(f'num items: {len(inv_list)}')
-    db.save_listings(eng, inv_list)
+    database.save_listings(eng, inv_list)
 
 
 def test_scrape_url():
-    eng = db.engine()
+    eng = database.engine()
     print('about to scrape against live site')
     for region in scrape.all_regions():
         inv_list = scrape.titan(region)
-        db.save_listings(eng, inv_list)
+        database.save_listings(eng, inv_list)
 
 
 if __name__ == '__main__':
