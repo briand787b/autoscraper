00967e436873b71e4fd6a5c78de4d0d602db8a30
Brian D'Ostilio <briand787b@gmail.com>
2023-02-12 01:42:24 +0000

    fixes systemd sript

--- autotrader/main.sh
+++ autotrader/main.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-source "${MAMBA_PATH:-'/home/brian/mambaforge'}/bin/activate autoscraper"
-
-## diagnostic stuff
-# mamba info
-# echo the script ran!!!
-# exit 0
+source /home/brian/mambaforge/etc/profile.d/conda.sh
+conda activate autoscraper
 
 python -u /home/brian/autoscraper/autotrader/main.py scrape \
 	--password "${AUTOTRADER_PW:-autotrader}"
